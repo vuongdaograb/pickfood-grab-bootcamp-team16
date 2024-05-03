bf3df8fa9b5f28453412db97df05a5f6e8e72b69
KitoKotowa <chaukiet220803@gmail.com>
2024-05-04 05:34:29 +0700

    Add proto UI

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { Croissant, HandMetal } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+      <div className='container flex items-center justify-between'>
+        <Link href='/'>
+          <Croissant />
+        </Link>
+        <p>PickFood</p>
+        <Link className={buttonVariants()} href='/signin'>
+          Sign in
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

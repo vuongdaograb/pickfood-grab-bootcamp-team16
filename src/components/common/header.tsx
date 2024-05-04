4b2549b02f3cf603bae152b7d3888a5b3dbe01e6
nxhanh <hanhxuan1804@gmail.com>
2024-05-04 14:00:31 +0700

    Add: header with dark mode toggle

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,9 +1,13 @@
+'use client'
+import DarkModeToggle from '@/components/ui/DarkModeToggle'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between'>
-        header
+    <header className='w-full flex items-center justify-between px-4 pt-2'>
+        <Image src='/logo.svg' alt='logo' width={150} height={150} />
+        <DarkModeToggle />
     </header>
   )
 }

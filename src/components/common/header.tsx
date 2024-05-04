a5fec17a992f852656147d685bdd56fdfbf99d5b
nxhanh <hanhxuan1804@gmail.com>
2024-05-04 12:44:30 +0700

    FE: config root layout, metadata, font, favicon, logo

---
+++ src/components/common/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex items-center justify-between'>
+        header
+    </header>
+  )
+}
+
+export default Header

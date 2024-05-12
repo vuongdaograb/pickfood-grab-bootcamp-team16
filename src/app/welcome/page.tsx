68ca417245915dd97db3a6cf224ab960caa1f0dd
Ngo Vo Quang Huy <20120295@student.hcmus.edu.vn>
2024-05-12 11:05:32 +0700

    Add api to get categries

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import MobileBody from "@/components/welcome/MobileBody";
+import StoryContainer from "@/components/welcome/StoryContainer";
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  const story = {
+    title: "Ăn ngon, ăn đúng, ăn vui!",
+    description:
+      "Băn khoăn ăn gì? Khám phá nhà hàng mới? Cùng PickFood, mỗi bữa ăn là niềm vui, kết nối và khởi đầu cho cuộc sống khỏe mạnh!",
+  };
+  return (
+    <main className="w-full flex flex-col items-center justify-center">
+      <MobileBody />
+      <StoryContainer title={story.title} story={story.description} />
+    </main>
+  );
+};
+
+export default page;
---
+++ src/components/home/AppMenu.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React, { FC, useEffect, useState } from "react";
+
+interface AppMenuProps {}
+
+const AppMenu: FC<AppMenuProps> = ({}) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [activeIndex, setActiveIndex ] = useState(2);
+  const items = [
+    { src: "/app-menu-all-icon.svg", alt: "all", link: "/all" , isDevelop: false },
+    { src: "/app-menu-temphobby-icon.svg", alt: "temp", link: "/temp-hobby", isDevelop: false},
+    { src: "/app-menu-home-icon.svg", alt: "home", link: "/home", size: 65 , isDevelop: true},
+    { src: "/app-menu-history-icon.svg", alt: "history", link: "/history", isDevelop: false },
+    { src: "/app-menu-user-icon.svg", alt: "user", link: "/user"  , isDevelop: true },
+  ];
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+  useEffect(() => {
+    const homePath = '/' + pathname.split("/")[1] ;
+    const index = items.findIndex((item) => item.link === homePath);
+    setActiveIndex(index != -1 ? index : 2);
+  }
+  // eslint-disable-next-line
+  , [pathname]);
+
+  return (
+    <div className="h-20 w-full max-w-screen-sm bg-[#111418] flex flex-row justify-between items-center px-8">
+      {items.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="cursor-pointer w-[65px] h-[65px] flex justify-center items-center rounded-full"
+            onClick={() => item.isDevelop ? handleClick(item.link) : null}
+          >
+            <Image
+              src={item.src}
+              alt={item.alt}
+              width={item.size || 35}
+              height={item.size || 35}
+              className={index === activeIndex ? "opacity-100" : "opacity-50"}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AppMenu;

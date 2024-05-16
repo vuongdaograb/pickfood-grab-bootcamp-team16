a2333d958515fbc5794b73a69be74fa30d31eb40
KitoKotowa <chaukiet220803@gmail.com>
2024-05-16 18:19:01 +0700

    Connect API to choose favoritefavorite

--- src/app/onboarding/breakfast/page.tsx
+++ src/app/onboarding/breakfast/page.tsx
@@ -1,56 +1,31 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Heart, Croissant } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 interface FoodItem {
+  id: number;
   name: string;
-}
-
-const foodList: FoodItem[] = [
-    { name: "Bánh mì pate" },
-    { name: "Bún chả" },
-    { name: "Bún bò Huế" },
-    { name: "Bún riêu cua" },
-    { name: "Bún thang" },
-    { name: "Cá kho tộ" },
-    { name: "Chả cá Lã Vọng" },
-    { name: "Chả giò" },
-    { name: "Chè hạt sen" },
-    { name: "Chè bưởi" },
-    { name: "Cơm gà Hội An" },
-    { name: "Cơm tấm sườn bì chả" },
-    { name: "Gỏi cuốn" },
-    { name: "Lẩu Thái" },
-    { name: "Lẩu mắm" },
-    { name: "Mì Quảng" },
-    { name: "Nem rán" },
-    { name: "Phở bò" },
-    { name: "Phở gà" },
-    { name: "Tôm rang muối" },
-    { name: "Thịt kho tàu" },
-    { name: "Bánh xèo" },
-    { name: "Bánh canh ghẹ" },
-    { name: "Bánh tráng cuốn thịt heo" },
-    { name: "Bánh bèo chén" },
-    { name: "Bánh ít lá gai" },
-    { name: "Bánh chuối nướng" },
-    { name: "Bánh flan" },
-    { name: "Bánh kem" },
-    { name: "Bánh bèo chén" },
-    { name: "Bánh ít lá gai" },
-    { name: "Bánh chuối nướng" },
-    { name: "Bánh flan" },
-    { name: "Bánh kem" },
-  ];
-
-interface AppState {
-  selectedFoodItems: FoodItem[];
 }
 
 const App: React.FC = () => {
   const [selectedFoodItems, setSelectedFoodItems] = useState<FoodItem[]>([]);
+  // const [categories, setCategories] = useState<FoodItem[]>([]);
+  const [foodList, setFoodList] = useState<FoodItem[] | any>([]);
+
+  const addFavorite = async () => {
+    const response = await fetch('/api/addfavor', {
+      method: 'POST',
+      headers : {
+        'Content-Type' : 'application/json',
+        // Authorization : localStorage.getItem("token")
+        Authorization : "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..H4ZrKxqVxmU4W3jBBjzZ_A.K60SCzydwLW23W7KwoU7Tm6rIAfjs4mSFPiGTbuDNsiYekXcS0dfmOKhvcq6AKUZ.uK5yEtE5Ap-GJvdNvIqClw"
+      },
+      // selectedFoodItems.push()
+    })
+    
+  }
 
   const handleFoodSelect = (chosenFood: FoodItem) => {
     setSelectedFoodItems((prevFoodItems) =>
@@ -64,71 +39,66 @@ const App: React.FC = () => {
 
   const getCategories = async () => {
     // console.log(values);
-    const router = useRouter();
     const response = await fetch('/api/getcategories', {
       method: 'GET',
       headers : {
-        'Content-Type' : 'application/json'
+        'Content-Type' : 'application/json',
+        // Authorization : localStorage.getItem("token")
+        Authorization : "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..H4ZrKxqVxmU4W3jBBjzZ_A.K60SCzydwLW23W7KwoU7Tm6rIAfjs4mSFPiGTbuDNsiYekXcS0dfmOKhvcq6AKUZ.uK5yEtE5Ap-GJvdNvIqClw"
       },
-      // body: JSON.stringify({
-      //   email: values.email,
-      //   password: values.password,
-      //   })
       })
-  
+
       if(response.ok) {
-        router.push('/home')
+        const result = (await response.json());
+        // result.then((jsonData: string) => result)
+        const myList: FoodItem[] = result.map((item) => ({ id: item[0], name: item[1] }));
+        console.log(myList);
+        // console.log(myList);
+        // console.log(Array.isArray(result));
+        return myList;
+        //router.push('/home')
       } else {
         console.error('Registration failed')
-      }
+      };
   };  
 
-  const getUserFavorite = async () => {
-    // console.log(values);
-    const router = useRouter();
-    const response = await fetch('api/addfavor', {
-      method: 'POST',
-      headers : {
-        'Content-Type' : 'application/json'
-      },
-      // body: JSON.stringify({
-      //   email: values.email,
-      //   password: values.password,
-      //   })
+  useEffect(() => {
+    // Using a Promise to simulate an asynchronous operation
+    getCategories()
+    .then((result) => {
+        setFoodList(result);
       })
-  
-      if(response.ok) {
-        router.push('/home')
-      } else {
+      .catch((error) => {
         console.error('Registration failed')
-      }
-  };  
+      });
+  }, []);
 
   return (
-    <div>
+    <div> 
       <div className="header">
           <h1 className="flex justify-center flex-wrap text-xl">Chọn những món bạn thích</h1>
       </div>
       <div className='flex flex-wrap justify-stretch'>
-
-      {foodList.map((food, index) => (
-          <Button 
-            variant ={`${selectedFoodItems.includes(food)? 'default' : 'outline'}`}
-            // variant = 'outline'
-            key={index}
-            className={`mt-1 mr-1 ml-1 ${selectedFoodItems.includes(food)? 'bg-red-400 border-2 border-red-400' : 'bg-green-600 border-2 border-green-600'}`}
-            type="submit"
-            onClick={() => handleFoodSelect(food)}
-          >
-            <div>
-              {selectedFoodItems.includes(food) ? <Heart className = 'mr-1'/> : <Croissant className = 'mr-1'/>}
-            </div>
-            {food.name}
-          </Button>
+      
+      {foodList.map((food: FoodItem, index) => (
+        <Button 
+          variant ={`${selectedFoodItems.includes(food)? 'default' : 'outline'}`}
+          // variant = 'outline'
+          key={index}
+          className={`mt-1 mr-1 ml-1 text-white ${selectedFoodItems.includes(food)? 'bg-red-400 border-2 border-red-400' : 'bg-green-600 border-2 border-green-600'}`}
+          type="submit"
+          onClick={() => handleFoodSelect(food)}
+        >
+          <div>
+            {selectedFoodItems.includes(food) ? <Heart className = 'mr-1'/> : <Croissant className = 'mr-1'/>}
+          </div>
+          {food.name}
+        </Button>
       ))}
+
       </div>
       <div className="footer">
-        <Button className='absolute w-11/12 mr-4 m-3 bottom-0 bg-green-600 justify-center' type='submit'>
+        <Button className='absolute bottom-3 left-3 right-3 text-white bg-green-600 justify-center' type='submit'>
           Tiếp tục
         </Button>
         </div>

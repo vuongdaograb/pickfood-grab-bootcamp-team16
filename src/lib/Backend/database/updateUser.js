45225e13c0a9fe8aed947980137fad07da1df032
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2024-05-17 20:02:10 +0700

    add: session data, reactdishes api

--- src/lib/Backend/database/updateUser.js
+++ src/lib/Backend/database/updateUser.js
@@ -1,11 +1,8 @@
 const User = require('@/models/userSchema.js');
-const database = require('@/lib/Backend/database/database.js');
+const database = require('@/lib/Backend/database/Database.js');
 
 
 async function updateFavorites(email, favorite_category) {
-    favorite_category = favorite_category.filter((item) => item < Number(process.env.MAX_CATEGORY));
-    let favorite_set = new Set(favorite_category);
-    favorite_category = Array.from(favorite_set);
     const update = {
         $set: {
             favorites: favorite_category

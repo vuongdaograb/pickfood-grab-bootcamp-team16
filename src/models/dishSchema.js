0146e2e45c735b9f695ea9fdc90a87fef44f131a
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2024-05-20 20:30:51 +0700

    Optimization: query performance

--- src/models/dishSchema.js
+++ src/models/dishSchema.js
@@ -8,10 +8,13 @@ const DishSchema = new mongoose.Schema({
     imgLink: String,
     price: Number,
     description: String,
-    merchant_id: String,
+    merchant_id: mongoose.Schema.Types.ObjectId,
     category: [String],
     category_id: Number,
     rank: Number
   });
+
+//   indexing the category_id field
+// DishSchema.index({ category_id: 1 });
 
 module.exports = mongoose.models.Dishes || mongoose.model('Dishes', DishSchema);

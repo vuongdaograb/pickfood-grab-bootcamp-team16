a58d037193923bf17a6f43a7ef17cf56152e538d
KitoKotowa <chaukiet220803@gmail.com>
2024-05-21 17:42:03 +0700

    Remove duplicates routing

--- src/components/home/Settings.tsx
+++ src/components/home/Settings.tsx
@@ -61,8 +61,6 @@ const SettingsPage = () => {
           document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
         });
 
-        router.push("/signin");
-
         router.push("/signin")
         console.log("Logout");
       };
